4d0bd44e6d04ab60861923f850a516b339b6be7e
oscaresteve <oscarestevea24@gmail.com>
2026-01-17 11:51:53 +0100

    Bug corregido para rutas publicas

--- src/router/router.js
+++ src/router/router.js
@@ -3,19 +3,27 @@ import { setRoute, getState } from "../state/store";
 //El router solo escucha cambios en el hash y modifica el estado
 export const router = {
   protectedRoutes: ["home"],
+  publicOnlyRoutes: ["login", "register"],
   init() {
     const handleHashchange = () => {
       let route = location.hash.replace("#", "") || "login";
-      
+
       console.log("Hash changed:", route);
-      
+
       const user = getState().user;
 
+      //Usuario no autenticado, redirige login
       if (this.protectedRoutes.includes(route) && !user) {
         route = "login";
         location.hash = "#login";
       }
-      
+
+      //Usuario autenticado, redirige a home
+      if (this.publicOnlyRoutes.includes(route) && user) {
+        route = "home";
+        location.hash = "#home";
+      }
+
       setRoute(route);
     };
     window.addEventListener("hashchange", handleHashchange);
